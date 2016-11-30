6f06382288ee23e1293471579d615e5d6b4b7f6b
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-30 10:49:53 -0700

    revert id to incremented value

--- src/server/db/migrations/20161122201420_users.js
+++ src/server/db/migrations/20161122201420_users.js
@@ -1,10 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
-    table.string('id')notNullable.unique();
+    table.increments()
     table.string('email').notNullable().unique();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
-    table.string('fb_obj')
+    table.string('fb_token').unique();
   });
 };
 
